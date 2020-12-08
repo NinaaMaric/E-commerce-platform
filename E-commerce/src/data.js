014f2e7e59853009550a701c63c13cdf2408ab3a
Nina <maric.nina@outlook.com'git config --global user.name 'NinaaMaric>
2020-12-08 14:05:08 +0100

    inital commit

---
+++ E-commerce/src/data.js
@@ -0,0 +1,70 @@
+export const data ={
+    products: [
+        {
+            _id: '1',
+            name: "Nike shirt",
+            category: "Shirts",
+            image: './img/product-1,jpg',
+            price: 120,
+            brand: "Nike",
+            rating: 4.5,
+            numReview: 10,
+            description: "high quality product"
+        },
+        {
+            _id: '2',
+            name: "Addidas shorts",
+            category: "Shorts",
+            image: './img/product-1,jpg',
+            price: 100,
+            brand: "Addidas",
+            rating: 4.0,
+            numReview: 14,
+            description: "high quality product"
+        },
+        {
+            _id: '3',
+            name: "Lacoste shirt",
+            category: "Shirts",
+            image: './img/product-1,jpg',
+            price: 150,
+            brand: "Lacoste",
+            rating: 4.5,
+            numReview: 11,
+            description: "high quality product"
+        },
+        {
+            _id: '4',
+            name: "Puma pants",
+            category: "Panst",
+            image: './img/product-1,jpg',
+            price: 70,
+            brand: "Puma",
+            rating: 3,
+            numReview: 7,
+            description: "high quality product"
+        },
+        {
+            _id: '5',
+            name: "Nike pants",
+            category: "Pants",
+            image: './img/product-1,jpg',
+            price: 120,
+            brand: "Nike",
+            rating: 5,
+            numReview: 15,
+            description: "high quality product"
+        },
+        {
+            _id: '6',
+            name: "Champion shirt",
+            category: "Shirts",
+            image: './img/product-1,jpg',
+            price: 80,
+            brand: "Champion",
+            rating: 3.5,
+            numReview: 9,
+            description: "high quality product"
+        },
+    ]
+}
